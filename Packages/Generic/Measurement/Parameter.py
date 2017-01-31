d2f1dfc4b69a9e698da1c71942eb354318fc303c
Michael Volk <michaelvolk1979@gmail.com>
2017-01-31 16:26:02 +0100

    mass now supports unit conversion

--- Packages/Generic/Measurement/Parameter.py
+++ Packages/Generic/Measurement/Parameter.py
@@ -1,6 +1,8 @@
 import pandas as pd
-from RockPy.core.measurement import Measurement
+
 import RockPy.core.utils
+from RockPy.core.measurement import Measurement
+import RockPy
 
 
 class Parameter(Measurement):
@@ -16,7 +18,6 @@ class Mass(Parameter):
     def __init__(self, sobj,
                  fpath=None, ftype='generic',
                  mass=None,
-                 std=None, time=None,
                  series=None,
                  **options):
 
@@ -24,7 +25,7 @@ class Mass(Parameter):
                                    fpath=fpath, ftype=ftype,
                                    series=series,
                                    **options)
-        unit = None
+        unit = 'kg'
 
         if isinstance(mass, str):
             mass, unit = RockPy.core.utils.split_num_alph(mass)
@@ -36,17 +37,69 @@ class Mass(Parameter):
                 raise IndexError(
                     '%s can not be converted into readable format: try (mass, unit) or \'mass unit\'' % mass)
 
+        # unit conversion into 'kg'
+        if unit:
+            mass *= RockPy.core.utils.convert[unit]['kg']
+
+
         self.unit = unit if unit else 'kg'
         data = pd.DataFrame(columns=['mass'], data=[[mass]])
         self.append_to_clsdata(data)
-        self.log().info('creating mass: %f, %s' % (mass, 'unit'))
+        self.log().info(
+            'creating mass: %.2e [%s] -> %.2e [%s]' % (mass * RockPy.core.utils.convert['kg'][unit], unit, mass, 'kg'))
 
-    def format_test(self):
-        pass
 
-    def format_testerich(self):
-        pass
+# class Length(Parameter):
+#     """
+#     simple 1d measurement for Length
+#     """
+#
+#     def __init__(self, sobj,
+#                  fpath=None, ftype='generic',
+#                  value=None, unit='m',
+#                  direction=None,
+#                  series=None,
+#                  **options):
+#         super(Length, self).__init__(sobj=sobj,
+#                                      fpath=fpath, ftype=ftype,
+#                                      series=series,
+#                                      **options)
+#         if not value:
+#             return
+#
+#         self.ftype = ftype
+#         self.direction = direction
+#
+#         # length_conversion = convert2(unit, 'm', 'length')
+#
+#         if not length_conversion:
+#             self.log().warning('unit << %s >> most likely not %s-compatible' % (unit, self.__class__.get_subclass_name()))
+#             self.log().error('CAN NOT create Measurement')
+#             return
+#
+#         self._data = {'data': RockPy3.Data(column_names=[self.mtype, 'time', 'std_dev'])}
+#         self._data['data'][self.mtype] = value * length_conversion
+#         self._data['data']['time'] = time
+#         self._data['data']['std_dev'] = std
+#
+#     def format_generic(self):
+#         pass
+#
+#
+# class Diameter(Length):
+#     """
+#     simple 1d measurement for Length
+#     """
+#
+#     pass
+#
+#
+# class Height(Length):
+#     """
+#     simple 1d measurement for Length
+#     """
+#     pass
 
 if __name__ == '__main__':
-    test = Mass('test', mass='23.7 mg', series=('test', 2, 'au'))
-    print(test.data)
+    s = RockPy.Sample('test', mass='1.3827ng')
+    print(s.measurements[0]._clsdata)
