525ff9ee1458bc14e2d17ed181531464ea92f438
Mike Volk <michaelvolk1979@gmail.com>
2017-03-15 12:18:48 -0500

    adds finished paleointensity basic import

--- Packages/Mag/Ftype/jr6.py
+++ Packages/Mag/Ftype/jr6.py
@@ -13,19 +13,20 @@ class jr6(Ftype):
     def __init__(self, dfile, snames=None, dialect=None, volume=10 ** -5):
         super().__init__(dfile, snames=snames, dialect=dialect)
 
-        xyz = ['x', 'y', 'z']
+        xyz = ['magn_x', 'magn_y', 'magn_z']
 
         data = pd.read_fwf(dfile, widths=(10, 8, 6, 6, 6, 4, 4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 4, 2),
-                           names=['specimen', 'step', 'x', 'y', 'z', 'exp',
+                           names=['specimen', 'step', 'magn_x', 'magn_y', 'magn_z', 'exp',
                                   'azimuth', 'dip',
                                   'foliation plane azimuth of dip', 'foliation plane dip',
                                   'lineation trend', 'lineation plunge',
                                   'P1', 'P2', 'P3', 'P4', 'Precision of measurement', 'CR-LF'
                                   ], usecols=[0, 1, 2, 3, 4, 5], comment='#')
 
+
         data[xyz] = [v * 10 ** data['exp'][i] for i, v in enumerate(data[xyz].values)]
         data[xyz] *= volume
-        data['m'] = np.linalg.norm(data[xyz], axis=1)
+        data['magn_moment'] = np.linalg.norm(data[xyz], axis=1)
         data['level'] = [20 if i == 'NRM' else round(float(i.replace("T", ''))) for i in data['step']]
         data['treat_temp'] = data['level'] + 273
         data['LT_code'] = [self.lookup_lab_treatment_code(i) for i in data['step']]
--- Packages/Mag/Measurement/paleointensity.py
+++ Packages/Mag/Measurement/paleointensity.py
@@ -19,8 +19,7 @@ import datetime
 from RockPy.core import measurement
 
 
-class Paleointensity(measurement.Measurement):
-
+class Paleointensity (measurement.Measurement):
     # def __init__(self, sobj,
     #              fpath=None, ftype=None, mdata=None,
     #              ftype_data = None,
@@ -43,47 +42,124 @@ class Paleointensity(measurement.Measurement):
         -------
 
         """
+
+        # check if sample name in file
         if sobj_name not in ftype_data.data['specimen'].values:
-            Paleointensity.log().error('CANNOT IMPORT -- sobj_name not in ftype_data specimen list.')
-            Paleointensity.log().error('wrong sample name?')
+            Paleointensity.log ().error ('CANNOT IMPORT -- sobj_name not in ftype_data specimen list.')
+            Paleointensity.log ().error ('wrong sample name?')
             return
 
-        data = ftype_data.data[ftype_data.data['specimen']==sobj_name].reset_index(drop=True)
+        # read ftype
+        data = ftype_data.data[ftype_data.data['specimen'] == sobj_name].reset_index (drop=True)
+
+        # rename the columns from magic format -> RockPy internal names
+        print (data)
+        data = data.rename (
+            columns={'magn_x': 'x', 'magn_y': 'y', 'magn_z': 'z', 'magn_moment': 'm', 'treat_temp': 'ti'})
+        print (data)
+
+        # add tj column:
+        # tj := temperature prior to ti step e.g. temperature before ck step
         data['tj'] = np.nan
-        data['tj'].iloc[1:] = data['treat_temp'].values[:-1]
+        data.loc[1:, ('tj')] = data['ti'].values[:-1]
+
+        # delete specimens column
         del data['specimen']
         return data
 
     @property
     def th(self):
-        d = self.data[(self.data['LT_code'] == 'LT-T-Z') | (self.data['LT_code'] == 'LT-NO')].set_index('treat_temp')
+        """
+        Thermal demagnetization steps of the experiments, also giving NRM step
+
+        Returns
+        -------
+            pandas.DataFrame
+        """
+        d = self.data[(self.data['LT_code'] == 'LT-T-Z') | (self.data['LT_code'] == 'LT-NO')].set_index ('ti')
         return d
 
     @property
     def pt(self):
-        d = self.data[(self.data['LT_code'] == 'LT-T-I') | (self.data['LT_code'] == 'LT-NO')].set_index('treat_temp')
-        return d\
+        """
+        Acquisition of partial Thermal remanent magnetization steps of the experiments, also giving NRM step.
+
+        Notes
+        -----
+        This gives the experimental value of the NRM remaining (ti) and pTRM acquisition (ti). The true pTRM gained (ti)
+        can be obtained with measurement.ptrm
+
+        Returns
+        -------
+            pandas.DataFrame
+        """
+        d = self.data[(self.data['LT_code'] == 'LT-T-I') | (self.data['LT_code'] == 'LT-NO')].set_index ('ti')
+        return d
 
     @property
     def ck(self):
-        d = self.data[self.data['LT_code'] == 'LT-PTRM-I'].set_index('treat_temp')
+        """
+        pTRM check steps of the experiments, also giving NRM step
+
+        Returns
+        -------
+            pandas.DataFrame
+        """
+
+        d = self.data[self.data['LT_code'] == 'LT-PTRM-I'].set_index ('ti')
+        return d\
+
+    @property
+    def ac(self):
+        """
+        aditivity check steps of the experiments, also giving NRM step
+
+        Returns
+        -------
+            pandas.DataFrame
+        """
+
+        d = self.data[self.data['LT_code'] == 'LT-PTRM-Z'].set_index ('ti')
+        return d\
+
+    @property
+    def tr(self):
+        """
+        MD tail check steps of the experiments, also giving NRM step
+
+        Returns
+        -------
+            pandas.DataFrame
+        """
+
+        d = self.data[self.data['LT_code'] == 'LT-PTRM-MD'].set_index ('ti')
         return d
 
     @property
     def ptrm(self):
-        d = deepcopy(self.pt)
-        d[['x','y','z']] = self.pt[['x','y','z']] - self.th[['x','y','z']]
+        """
+        pTRM acuisition steps of the experiments. Vector substration of the pt[[x,y,z]] and th[[x,y,z]] steps for
+        each ti. Also recalculates the moment ['m'] using np.linalg.norm
+
+        Returns
+        -------
+            pandas.DataFrame
+        """
+
+        d = self.pt.copy()
+        d.loc[:, ('x', 'y', 'z')] -= self.th.loc[:, ('x', 'y', 'z')]
         d['LT_code'] = 'PTRM'
-        d['m'] = np.linalg.norm(d[['x','y','z']], axis=1)
+        d['m'] = np.linalg.norm (d[['x', 'y', 'z']], axis=1)
 
         return d
 
-    ####################################################################################################################
+        ####################################################################################################################
+
 
 if __name__ == '__main__':
-    s = RockPy.Sample('61')
-    m = s.add_measurement(mtype='paleointensity',
-                          ftype='jr6',
-                          fpath=os.path.join(RockPy.test_data_path, 'TT-paleointensity.jr6'),
-                          dialect='tdt')
-    print(m.ptrm)
+    s = RockPy.Sample ('61')
+    m = s.add_measurement (mtype='paleointensity',
+                           ftype='jr6',
+                           fpath=os.path.join (RockPy.test_data_path, 'TT-paleointensity.jr6'),
+                           dialect='tdt')
+    print (m.tr)
