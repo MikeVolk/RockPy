f4ed567f876e723aaa57ccdae4b85e25df2b9173
Mike Volk <michaelvolk1979@gmail.com>
2017-04-19 16:45:23 -0500

    started testing for paleointensity

---
+++ RockPy/tests/test_paleointensity.py
@@ -0,0 +1,30 @@
+from unittest import TestCase
+
+
+class TestPaleointensity(TestCase):
+    def test_equal_acqu_demag_steps(self):
+        self.fail()
+
+    def test_from_simulation(self):
+        self.fail()
+
+    def test_format_jr6(self):
+        self.fail()
+
+    def test_zf_steps(self):
+        self.fail()
+
+    def test_if_steps(self):
+        self.fail()
+
+    def test_ck(self):
+        self.fail()
+
+    def test_ac(self):
+        self.fail()
+
+    def test_tr(self):
+        self.fail()
+
+    def test_ifzf_diff_steps(self):
+        self.fail()
