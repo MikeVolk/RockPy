1d5abe54fdf03a24d7556791fe5f6fabca091c25
Mike Volk <michaelvolk1979@gmail.com>
2017-04-25 10:57:53 -0500

    adds mtype_implemented function

--- RockPy/core/utils.py
+++ RockPy/core/utils.py
@@ -47,6 +47,20 @@ def ignored(*exceptions):
     except exceptions:
         pass
 
+def mtype_implemented(mtype):
+    """
+    Function to check if mtype is implemented
+    
+    Parameters
+    ----------
+    mtype: str
+        mtype to check
+
+    Returns
+    -------
+        bool
+    """
+    return True if mtype in RockPy.implemented_measurements else False
 
 def tuple2list_of_tuples(item) -> list:
     """
