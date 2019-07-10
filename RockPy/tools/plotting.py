3cbe7315af3d8e63a7b54ff39715759140e04cea
Michael Volk <michaelvolk1979@gmail.com>
2019-07-10 11:33:42 -0400

    adds MidPointNormalize normalizer

--- RockPy/tools/plotting.py
+++ RockPy/tools/plotting.py
@@ -8,6 +8,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import colors as mcol
 from matplotlib.collections import LineCollection
+import matplotlib.colors as colors
 
 colorpalettes = {
     'cat10': ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22',
@@ -363,6 +364,23 @@ def connect_ax_data(ax, **kwargs):
 
 
 red_blue_colormap = mcol.LinearSegmentedColormap.from_list("MyCmapName", ["b", "r"])
+
+# set the colormap and centre the colorbar
+class MidpointNormalize(colors.Normalize):
+    """
+    Normalise the colorbar so that diverging bars work there way either side from a prescribed midpoint value)
+
+    e.g. im=ax1.imshow(array, norm=MidpointNormalize(midpoint=0.,vmin=-100, vmax=100))
+    """
+    def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+        self.midpoint = midpoint
+        colors.Normalize.__init__(self, vmin, vmax, clip)
+
+    def __call__(self, value, clip=None):
+        # I'm ignoring masked values and all kinds of edge cases to make a
+        # simple example...
+        x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
+        return np.ma.masked_array(np.interp(value, x, y), np.isnan(value))
 
 
 """ SPECIAL """
